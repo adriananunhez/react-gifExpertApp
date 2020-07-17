a401e2230b44617b6465469f487ab261d92995e9
Adriana Nunez <adri.nunhez@gmail.com>
2020-07-17 17:32:01 -0400

    Docs add

---
+++ docs/precache-manifest.e034415ca076b8ca2f6558f4de6ed1bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a435e763eb587b070e99bd4ee429b269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da713138a5c5f3d14343",
+    "url": "/static/css/main.5d5a1a2f.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da713138a5c5f3d14343",
+    "url": "/static/js/main.b74d31e8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
